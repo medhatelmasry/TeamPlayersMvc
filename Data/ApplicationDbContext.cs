0d134223e6cfddef3eecbaea2b2bae08fa38f695
medhatelmasry <medhatelmasry@live.com>
2023-09-25 18:40:36 -0700

    1st

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TeamPlayersMvc.Models;
+
+namespace TeamPlayersMvc.Data;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+
+    public DbSet<Team>? Teams { get; set; }
+    public DbSet<Player>? Players { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<Player>().Property(m => m.TeamName).IsRequired();
+
+        builder.Entity<Team>().Property(p => p.TeamName).HasMaxLength(30);
+
+        builder.Entity<Team>().ToTable("Team");
+        builder.Entity<Player>().ToTable("Player");
+
+        builder.Seed();
+    }
+}
